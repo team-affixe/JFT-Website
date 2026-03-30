9a1a304212dd065301e26b099d7ef755462a8575
team-affixe <team.affixe-7v@icloud.com>
2026-03-30 16:50:11 +0200

    Add favicon

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
 if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
   // Dark Mode aktivieren
+  document.body.classList.add("dark-mode");
   document.querySelectorAll(".body").forEach(el => el.classList.add("dark-mode"));
   document.querySelectorAll(".content-box").forEach(el => el.classList.add("dark-mode"));
 }
 
 window.matchMedia("(prefers-color-scheme: dark)").addListener((e) => {
   if (e.matches) {
+    document.body.classList.add("dark-mode");
     document.querySelectorAll(".body").forEach(el => el.classList.add("dark-mode")); // Dark Mode einschalten
     document.querySelectorAll(".content-box").forEach(el => el.classList.add("dark-mode"));
   } else {
+    document.body.classList.remove("dark-mode");
     document.querySelectorAll(".body").forEach(el => el.classList.remove("dark-mode")); // Dark Mode ausschalten
     document.querySelectorAll(".content-box").forEach(el => el.classList.remove("dark-mode"));
   }
