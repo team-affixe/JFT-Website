4132b3fa805f08f9a1f5f624dddcb4cb2b38184f
team-affixe <team.affixe-7v@icloud.com>
2026-04-11 10:11:26 +0200

    Add Pop-Up Window

--- script.js
+++ script.js
@@ -53,5 +53,12 @@ function OpenImpressum() {
   window.open("impressum.html", "_blank");
 }
 function menuToggle() {
-  document.getElementById('menuToggle').checked = false;
+  document.getElementById("menuToggle").checked = false;
+}
+function ProjectPopUp() {
+  document
+    .querySelectorAll(".new-project-box")
+    .forEach((el) => el.classList.add("zoom"));
+  NewProjectBox.innerHTML =
+    '<h2 class="content-headline">Projekt einreichen:</h2><input type="text" name="name" size="20"><textarea id="w3review" name="w3review" rows="4" cols="50">At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies.</textarea><button class="submit-btn">Absenden</button>';
 }
