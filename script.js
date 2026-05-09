e9a6ccaaca83752726ab4bfb3eae8ebb8ff795c6
team-affixe <team.affixe-7v@icloud.com>
2026-05-09 18:13:06 +0200

    Redesigned Buttons and text area

--- script.js
+++ script.js
@@ -60,5 +60,12 @@ function ProjectPopUp() {
     .querySelectorAll(".new-project-box")
     .forEach((el) => el.classList.add("zoom"));
   NewProjectBox.innerHTML =
-    '<h2 class="content-headline">Projekt einreichen:</h2><input type="text" name="name" size="20"><textarea id="w3review" name="w3review" rows="4" cols="50">At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies.</textarea><button class="submit-btn">Absenden</button>';
+    '<h2 class="content-headline">Projekt einreichen:</h2><input type="text" style="background-color: #fafafaff; border-bottom: #ccc solid 2px; border-left: #ccc solid 1px; border-right: none; border-top: none;" name="name" size="20"><br><br><textarea id="w3review" name="w3review" rows="4" cols="50"style="background-color: #fafafaff; border-bottom: #ccc solid 2px; border-left: #ccc solid 1px; border-right: none; border-top: none;">At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies.</textarea><br><br><button onclick="SendButton()" class="submit-btn">Absenden</button>';
+}
+function SendButton() {
+    document
+    .querySelectorAll(".new-project-box")
+    .forEach((el) => el.classList.add("zoom"));
+  NewProjectBox.innerHTML =
+    '<h2 class="content-headline">Projekt einreichen:</h2><input type="text" style="background-color: #fafafaff; border-bottom: #ccc solid 2px; border-left: #ccc solid 1px; border-right: none; border-top: none;" name="name" size="20"><br><br><textarea id="w3review" name="w3review" rows="4" cols="50"style="background-color: #fafafaff; border-bottom: #ccc solid 2px; border-left: #ccc solid 1px; border-right: none; border-top: none;">At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies.</textarea><br><br><button onclick="SendButton()" class="submit-btn">Absenden</button>';
 }
